970c816fe6202c5cb2b8e9d7595461c734d2dc6d
Juliano cassimiro dos santos <julianocassimiro212@gmail.com>
2022-12-29 08:55:11 -0300

    Initial commit

---
+++ Php/formulario.php
@@ -0,0 +1,22 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+    
+        include_once('config.php');
+
+        $nome            = $_POST['nome'];
+        $senha           = $_POST['senha'];
+        $email           = $_POST['email'];
+        $telefone        = $_POST['telefone'];
+       
+       
+    
+    
+    
+
+        $result = mysqli_query($conexao, "INSERT INTO tb01_cadastro(nome,senha,email,telefone) 
+        VALUES ('$nome','$senha','$email','$telefone')");
+
+    }
+    ?>
